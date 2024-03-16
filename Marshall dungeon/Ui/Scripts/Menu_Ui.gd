10f63bd0f07f198cee301b2008a7a23cfc0fc38e
Martin Damasio <58669934+martdam@users.noreply.github.com>
2024-03-16 15:07:19 -0300

    act 8.6
    incluir git ignore y creditos

--- Marshall dungeon/Ui/Scripts/Menu_Ui.gd
+++ Marshall dungeon/Ui/Scripts/Menu_Ui.gd
@@ -3,6 +3,7 @@ extends Control
 
 func _ready():
 	get_node("AnimationPlayer").play("fade_in")
+	get_node("Panel_creditos").visible=false
 
 func _on_Salir_pressed():
 	get_tree().quit();
@@ -24,3 +25,11 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 	match anim_name:
 		"fade-out":
 			get_tree().change_scene("res://Levels/Tutorial.tscn")
+
+
+func _on_Creditos_pressed():
+	get_node("Panel_creditos").visible=true
+
+
+func _on_Cerrar_pressed():
+	get_node("Panel_creditos").visible=false
